864590de70e6577d35e4cccef5e80db4691d9763
BahmanKarimi0 <bahmankarimi0@gmail.com>
2025-03-09 08:18:14 +1200

    Add exercise 4 solution

--- JSON-Handling/04_json_copy.py
+++ JSON-Handling/04_json_copy.py
@@ -1,9 +1,9 @@
 import json
 try:
     with open("data.json", "r", encoding="utf-8") as rf, open("data_copy.json", "w", encoding="utf-8") as wf:
-        for line in rf:
-            print(line, file=wf)
-        print("File data.json copied successfully")
+        read_data = json.load(rf)
+        json.dump(read_data, wf, indent=4)
+        print("File data.json copied to data_copy.json successfully")
 except FileNotFoundError:
     print("File not found")
 except json.decoder.JSONDecodeError:
@@ -12,4 +12,3 @@ except PermissionError:
     print("Permission denied")
 except Exception as e:
     print(f"Error: {e}")
-
